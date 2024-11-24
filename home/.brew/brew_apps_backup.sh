c053af71b73fa37d6a53e461982217c70bd37ce8
Matei Pinzaru <matei@avocado.solutions>
2024-11-24 23:12:59 +0200

    switched from brew install scripts to 'brew bunlde'

--- home/.brew/brew_apps_backup.sh
+++
@@ -1,3 +0,0 @@
-# Brew backup
-brew leaves > ~/.homesick/repos/dotfiles/home/.brew/brew_leaves.txt
-brew list --casks > ~/.homesick/repos/dotfiles/home/.brew/brew_casks.txt
--- home/.brew/brew_apps_install.sh
+++
@@ -1,3 +0,0 @@
-# Brew restore backup
-xargs brew install --formula < brew_leaves.txt
-xargs brew install --cask < brew_casks.txt
